01dc03f2fb654f10751c8c5eb1fe60bafa1822d3
Muhammad Rizki Malik Aziz <rizkimalik221001@gmail.com>
2024-03-02 16:57:47 +0700

    feat: Add contact app component and styling

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id:1,
+            name:"Rizki",
+            tag: "Web Developer",
+            imageUrl: "https://source.unsplash.com/300x300/?avatars"
+        },
+        {
+            id:2,
+            name:"Malik",
+            tag: "UI/UX Design",
+            imageUrl: "https://source.unsplash.com/320x320/?avatars"
+        },
+        {
+            id:3,
+            name:"Aziz",
+            tag: "Ai Engineer",
+            imageUrl: "https://source.unsplash.com/340x340/?avatars"
+        },
+    ]
+}
+
+export { getData };
