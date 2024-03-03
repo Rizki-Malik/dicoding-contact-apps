cb4c099c84dd81cd6e244ad5e41fedba7e53d255
Muhammad Rizki Malik Aziz <rizkimalik221001@gmail.com>
2024-03-03 19:02:05 +0700

    feat: Implement search functionality with path and query parameters
    - Add a SearchBar component with input for search queries
    - Utilize React Router for path parameters (e.g., /?keyword)
    - Handle optional query parameters (e.g., /?keyword=Malik)
    - Replace navigation text with React Icons for a more visual UI

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { FiHome, FiPlusCircle } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 
 function Navigation(){
     return(
         <nav className="navigation">
             <ul>
-                <li><Link to={"/"}>Beranda</Link></li>
-                <li><Link to={"/add"}>Tambah</Link></li>
+                <li><Link to={"/"}><FiHome/></Link></li>
+                <li><Link to={"/add"}><FiPlusCircle/></Link></li>
             </ul>
         </nav>
     );
